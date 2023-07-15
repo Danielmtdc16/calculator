f2c1efc30e48261cadee2985995f61b38d0c0a3f
Daniel <danielmtdc16@gmail.com>
2023-07-15 19:38:31 -0300

    declarando as variáveis no script.

---
+++ assets/script/script.js
@@ -0,0 +1,8 @@
+const display_results = document.querySelector(".display-results");
+
+const delete_button = document.querySelector("#delete");
+const division = document.querySelector("#division");
+const multiplication = document.querySelector("#multiplication");
+const minus = document.querySelector("#minus");
+const plus = document.querySelector("#plus");
+const equal = document.querySelector("#equal");
