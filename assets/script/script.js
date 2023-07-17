b3c863d6de617f9db456463484dbc708a72fe21d
Daniel <danielmtdc16@gmail.com>
2023-07-17 12:03:25 -0300

    função para processar operações funcionando.

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,13 +12,50 @@ class Calculator {
         this.current_operation = "";
     }
 
-    addNumbers(number) {
-        this.current_operation += number;
+    addNumbers(digit) {
+        if (digit === "." && this.current_operation_text.innerText.includes(".")){
+            return;
+        } 
+        this.current_operation += digit;
         this.refreshScreen();
     }
 
-    refreshScreen() {
-        this.current_operation_text.innerText = this.current_operation;
+    addition(num1, num2) {
+        return num1 + num2;
+    }
+    
+    processOperation(operation) {
+        
+        let resultOperation;
+        const previous = +this.previous_operation_text.innerText.split(" ")[0];
+        const current = +this.current_operation_text.innerText;
+
+        switch(operation)
+         {
+            case "+":
+                resultOperation = previous + current;
+                this.refreshScreen(resultOperation, operation, current, previous);
+                break;
+                
+            default:
+                return;
+        }
+    }
+
+    refreshScreen(resultOperation = null, operation = null, current = null, previous = null) {
+        console.log(resultOperation, operation, current, previous);
+
+        if (resultOperation === null) {
+            this.current_operation_text.innerText = this.current_operation;
+        }
+        else {
+            if (previous === 0) {
+                resultOperation = current;
+            }
+            this.previous_operation_text.innerText = `${resultOperation} ${operation}`;
+            this.current_operation = "";
+        }
+
     }
 
     clearScreen() {
@@ -35,11 +72,10 @@ buttons.forEach(btn => {
     btn.addEventListener("click", () => {
         const value = btn.textContent;
         
-        if (+value >= 0 || value === ",") {
-            calculator.addNumbers(value)
+        if (+value >= 0 || value === ".") {
+            calculator.addNumbers(value);
         } else {
-            if (value === "C") calculator.clearScreen();
-            
+            calculator.processOperation(value);
         }
     });
 
